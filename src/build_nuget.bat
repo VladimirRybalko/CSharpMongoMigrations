1e39152a348d4b7624215756a6d6a3319534f503
Enrique Raso Barbero <erasobarbero@omp.com>
2021-06-17 16:14:24 +0200

    Fix issue with assembly and NuGet package version

--- src/build_nuget.bat
+++ src/build_nuget.bat
@@ -1,2 +1,5 @@
+rem --
+rem -- We can force the version of build and NuGet package with '-p:version=', but it is better to update it in .csproj file
+rem --
 dotnet build -c Release
 dotnet pack CSharpMongoMigrations/CSharpMongoMigrations.csproj /p:NuspecFile=../CSharpMongoMigrations.nuspec --output ../
